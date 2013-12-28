85242a6efe4af744c7170f155770cb6b8e2806e2
Phil Kulak <pkulak@gmail.com>
2013-12-28 11:58:39 -0800

    Get rid of the auto-launch code. It's just causing problems.

--- js/background.js
+++ js/background.js
@@ -1,6 +1,4 @@
-var deviceConnected = true;
-
-var openWindow = function() {
+chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('main.html', {
     id: "main-window",
     bounds: {
@@ -8,30 +6,4 @@ var openWindow = function() {
       height: 600
     }
   });
-}
-
-var checkForMedia = function() {
-  chrome.mediaGalleries.getMediaFileSystems({interactive: "if_needed"}, function(filesystems) {
-    var found = false;
-
-    for (i = 0; i < filesystems.length; i++) {
-      var fs = filesystems[i];
-      var info = chrome.mediaGalleries.getMediaFileSystemMetadata(fs);
-
-      if (info.isRemovable) {
-        found = true;
-        
-        if (!deviceConnected) {
-          openWindow();
-        }
-      }
-    }
-
-    deviceConnected = found;
-
-    setTimeout(checkForMedia, 3000);
-  });
-}
-
-chrome.app.runtime.onLaunched.addListener(openWindow);
-checkForMedia();
+});
